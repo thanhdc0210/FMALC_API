127795736bfaeb75281d73e912cf6664d8ab4a59
thanhdc63414 <dangcongthanh1998@gmail.com>
2020-06-02 11:47:50 +0700

    no message

---
+++ src/main/java/fmalc/api/FmalcApplication.java
@@ -0,0 +1,13 @@
+package fmalc.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FmalcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fmalc/api/FmalcApplicationTests.java
@@ -0,0 +1,13 @@
+package fmalc.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FmalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
