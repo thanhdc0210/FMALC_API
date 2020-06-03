5716ebf0307e0ac502a9f3103e6b39e631fb5049
thanhdc63414 <dangcongthanh1998@gmail.com>
2020-06-03 17:35:44 +0700

    no message

--- src/main/java/fmalc/api/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package fmalc.api.repository;
-
-import fmalc.api.entities.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface RoleRepository extends JpaRepository<Role, Integer>, JpaSpecificationExecutor<Role> {
-
-}
