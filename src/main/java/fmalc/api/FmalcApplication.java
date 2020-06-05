7c3f46c7dddb4881efadeb02f7889ab2282b1c7d
giangtlb <giangtlb97@gmail.com>
2020-06-05 23:53:40 +0700

    - Thêm swagger ( pom.xml) - Thêm base response

--- src/main/java/fmalc/api/FmalcApplication.java
+++ src/main/java/fmalc/api/FmalcApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @ComponentScan({"fmalc.api"})
 @EntityScan({"fmalc.api"})
 @EnableJpaRepositories({"fmalc.api.repository"})
+@EnableSwagger2
 public class FmalcApplication {
 
 	public static void main(String[] args) {
