a9df87423eef3e46295cbe1a38201c655bafec07
Nu Nguyen <ngtuyetnu1997@gmail.com>
2020-07-05 01:02:45 +0700

    crud

---
+++ src/main/java/fmalc/api/dto/FLeetManagerResponseDTO.java
@@ -0,0 +1,34 @@
+package fmalc.api.dto;
+
+import fmalc.api.entity.*;
+import lombok.Data;
+import org.modelmapper.ModelMapper;
+
+import java.sql.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Data
+public class FLeetManagerResponseDTO {
+    private Integer id;
+    private String identityNo;
+    private String name;
+    private String phoneNumber;
+    private Date dateOfBirth;
+    private String image;
+
+
+    public FLeetManagerResponseDTO mapToResponse(FleetManager fleetManager) {
+        ModelMapper modelMapper = new ModelMapper();
+        FLeetManagerResponseDTO fLeetManagerResponseDTO = modelMapper.map(fleetManager, FLeetManagerResponseDTO.class);
+        return fLeetManagerResponseDTO;
+    }
+
+    public List<FLeetManagerResponseDTO> mapToListResponse(List<FleetManager> fleetManagers) {
+        return fleetManagers
+                .stream()
+                .map(fleetManager -> mapToResponse(fleetManager))
+                .collect(Collectors.toList());
+    }
+
+}
