aa1f678b3e926e9a21ec8a04e2ec23c7db05cf1a
Nu Nguyen <ngtuyetnu1997@gmail.com>
2020-07-04 22:48:13 +0700

    crud

--- src/main/java/fmalc/api/controller/DriverController.java
+++ src/main/java/fmalc/api/controller/DriverController.java
@@ -55,6 +55,7 @@ public class DriverController {
             return ResponseEntity.badRequest().body(driver);
         }
     }
+
     @PutMapping(value = "id/{id}")
     public ResponseEntity<DriverResponseDTO> updateDriver(@PathVariable("id") Integer id, @RequestBody DriverRequestDTO driverRequest) {
        try {
--- src/main/java/fmalc/api/service/FleetManagerService.java
+++ src/main/java/fmalc/api/service/FleetManagerService.java
@@ -1,9 +1,18 @@
 package fmalc.api.service;
 
+import fmalc.api.dto.FleetManagerRequestDTO;
 import fmalc.api.entity.FleetManager;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface FleetManagerService {
     List<FleetManager> getAllFleet();
+
+    FleetManager save(FleetManagerRequestDTO fleetManagerRequestDTO, MultipartFile file) throws IOException;
+
+    FleetManager findById(Integer id);
+
+    FleetManager update(Integer id, FleetManagerRequestDTO fleetManagerRequestDTO);
 }
