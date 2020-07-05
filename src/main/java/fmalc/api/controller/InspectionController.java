3fc9a5da8e470fc2ad19229b4fbb551792c42241
Nu Nguyen <ngtuyetnu1997@gmail.com>
2020-07-06 00:34:17 +0700

    fleet api vs inspection api

---
+++ src/main/java/fmalc/api/controller/InspectionController.java
@@ -0,0 +1,58 @@
+package fmalc.api.controller;
+
+import fmalc.api.dto.DriverRequestDTO;
+import fmalc.api.dto.DriverResponseDTO;
+import fmalc.api.entity.Driver;
+import fmalc.api.entity.Inspection;
+import fmalc.api.service.InspectionService;
+import io.swagger.models.auth.In;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1.0/inspection")
+public class InspectionController {
+    @Autowired
+    InspectionService inspectionService;
+
+    @GetMapping
+    public ResponseEntity<List<Inspection>> getAllInspection() {
+        List<Inspection> result = inspectionService.findAll();
+        if (result.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok().body(result);
+    }
+
+    @GetMapping(value = "id/{id}")
+    public ResponseEntity<Inspection> findById(@PathVariable("id") Integer id) {
+        Inspection result = inspectionService.findById(id);
+        if (result == null) {
+            return ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.ok().body(result);
+    }
+
+    @PostMapping
+    public ResponseEntity createInspection(@RequestBody Inspection inspection) {
+        try {
+            inspection = inspectionService.save(inspection);
+            return ResponseEntity.ok().body(inspection);
+        } catch (Exception ex) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @PutMapping(value = "id/{id}")
+    public ResponseEntity<Inspection> updateInspection(@PathVariable("id") Integer id, @RequestBody Inspection inspection) {
+        try {
+            inspection = inspectionService.update(id, inspection);
+            return ResponseEntity.ok().body(inspection);
+        } catch (Exception ex) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+}
--- src/main/java/fmalc/api/repository/FleetManagerRepository.java
+++ src/main/java/fmalc/api/repository/FleetManagerRepository.java
@@ -7,10 +7,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.sql.Date;
+
 @Repository
 public interface FleetManagerRepository extends JpaRepository<FleetManager, Integer> {
     @Modifying
     @Transactional
-    @Query(value = "Update fleet_manager f set f.name = :name, f.identity_no = :identityNo  where d.id =:id", nativeQuery = true)
-    void updateFleetManager(Integer id, String name, String identityNo);
+    @Query(value = "Update fleet_manager f set f.name = :name, f.identity_no = :identityNo, f.date_of_birth = :dateOfBirth  where f.id =:id", nativeQuery = true)
+    void updateFleetManager(Integer id, String name, String identityNo, Date dateOfBirth);
 }
