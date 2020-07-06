4a814416228909de06d2d035173bb3bef573c9db
thanhdc63414 <dangcongthanh1998@gmail.com>
2020-07-07 01:45:49 +0700

    update time of token

--- src/main/java/fmalc/api/constant/SecurityConstant.java
+++ src/main/java/fmalc/api/constant/SecurityConstant.java
@@ -2,7 +2,7 @@ package fmalc.api.constant;
 
 public class SecurityConstant {
     public static final String SECRET = "secretKey";
-    public static final long EXPIRATION_TIME = 20*24*60*60; // 10 days
+    public static final long EXPIRATION_TIME = 10*24*60*60;
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
     public static final String AUTH_LOGIN_URL = "/api/v1.0/login";
