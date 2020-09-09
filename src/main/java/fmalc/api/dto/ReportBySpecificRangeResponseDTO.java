6fb5bd97845ad813d3050a6b34b20015a930449e
giangtlb <giangtlb97@gmail.com>
2020-09-09 16:27:06 +0700

    Thêm report trung bình lít/100km cho xe trong năm

--- src/main/java/fmalc/api/dto/ReportBySpecificRangeResponseDTO.java
+++ src/main/java/fmalc/api/dto/ReportBySpecificRangeResponseDTO.java
@@ -3,10 +3,14 @@ package fmalc.api.dto;
 
 import lombok.Data;
 
+import java.util.HashMap;
+import java.util.logging.Handler;
+
 @Data
 public class ReportBySpecificRangeResponseDTO {
 
     private Integer totalConsignment;
     private Integer rate;
+    private HashMap<Integer, Double> averageFuelByMonth;
 
 }
