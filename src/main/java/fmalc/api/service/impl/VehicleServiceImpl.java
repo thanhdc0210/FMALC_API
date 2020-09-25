2641dc6989dc8ee6c41c03adcf74f5a27f055982
thanhdc63414 <dangcongthanh1998@gmail.com>
2020-09-25 10:18:33 +0700

    Tài xế sau khi hoàn thành lô hàng có tối đa 2 ngày để báo cáo

--- src/main/java/fmalc/api/service/impl/VehicleServiceImpl.java
+++ src/main/java/fmalc/api/service/impl/VehicleServiceImpl.java
@@ -21,7 +21,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -773,7 +776,16 @@ public class VehicleServiceImpl implements VehicleService {
             });
 
             if (schedules.size() > 0){
-                return schedules.get(schedules.size()-1).getVehicle().getLicensePlates();
+                Schedule schedule = schedules.get(schedules.size()-1);
+                List<Place> places = new ArrayList<Place>(schedule.getConsignment().getPlaces());
+                Timestamp actualTime = places.get(places.size() - 1).getActualTime();
+                Timestamp startDate = Timestamp.valueOf(LocalDateTime.now().with(LocalTime.MIN).minusDays(2));
+                Timestamp endDate = Timestamp.valueOf(LocalDateTime.now().with(LocalTime.MAX));
+                if (actualTime.after(startDate) && actualTime.before(endDate)) {
+                    return schedule.getVehicle().getLicensePlates();
+                }else{
+                    return null;
+                }
             }else{
                 return null;
             }
